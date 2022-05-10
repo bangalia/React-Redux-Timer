23bb7eb9e5ec4fcc2552fe5e78091ea2de2acf4d
bangalia <anitabangali@yahoo.com>
2022-05-09 23:12:33 -0500

    added Timer reducers

--- tmrz-app/src/App.js
+++ tmrz-app/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component }  from 'react';
+import { createStore } from "redux"
+import { Provider } from "react-redux"
+import "./App.css"
+import reducers from "./reducers"
+import NewTimer from "./components/NewTimer"
+import ListTimers from "./components/ListTimers"
+import { update } from "./actions"
+import throttle from "lodash/throttle"
+import { loadState, saveState } from "./utils"
+
+const persistedState = loadState()
+const store = createStore(reducers, persistedState)
+store.subscribe(
+  throttle(() => {
+    saveState(store.getState())
+  }, 1000)
+)
+
+let lastUpdateTime = Date.now()
+setInterval(() => {
+  const now = Date.now()
+  const deltaTime = now - lastUpdateTime
+  lastUpdateTime = now
+  store.dispatch(update(deltaTime))
+}, 50)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Provider store={store}>
+      <div className="App">
+        <h1>TMRZ</h1>
+        <NewTimer />
+        <ListTimers />
+      </div>
+    </Provider>
+  )
 }
 
-export default App;
+export default App
